c48c4e76d18f7bf9cca59e8fa766ac16f77604fd
MrFr3di <ynagi-wm@mail.ru>
2026-09-21 12:21:30 +0500

    test(media-lab): add structured request tracing

---
+++ tools/media-lab/src/main/java/io/github/definitelystable/spongetube/medialab/TraceOutcome.java
@@ -0,0 +1,10 @@
+package io.github.definitelystable.spongetube.medialab;
+
+enum TraceOutcome {
+    SUCCESS,
+    NOT_FOUND,
+    METHOD_NOT_ALLOWED,
+    RANGE_UNSATISFIABLE,
+    CLIENT_DISCONNECTED,
+    SERVER_IO_ERROR
+}
