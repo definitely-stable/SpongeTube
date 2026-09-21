b1b0015cbe0c6c6f3350319a6001075752e06e20
MrFr3di <ynagi-wm@mail.ru>
2026-09-21 12:20:49 +0500

    test(media-lab): add safe fixture and range contracts

---
+++ tools/media-lab/src/main/java/io/github/definitelystable/spongetube/medialab/RangeDecision.java
@@ -0,0 +1,22 @@
+package io.github.definitelystable.spongetube.medialab;
+
+sealed interface RangeDecision permits RangeDecision.Full, RangeDecision.Partial, RangeDecision.Unsatisfiable {
+
+    record Full() implements RangeDecision {
+    }
+
+    record Partial(long startInclusive, long endExclusive) implements RangeDecision {
+        Partial {
+            if (startInclusive < 0 || endExclusive <= startInclusive) {
+                throw new IllegalArgumentException("Invalid partial range");
+            }
+        }
+
+        long length() {
+            return endExclusive - startInclusive;
+        }
+    }
+
+    record Unsatisfiable() implements RangeDecision {
+    }
+}
