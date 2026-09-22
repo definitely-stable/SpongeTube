4cdef5236ae1bdfc41182b4ee5ac5155085972a6
MrFr3di <ynagi-wm@mail.ru>
2026-09-22 14:48:08 +0500

    refactor: install durable extent under store root lease

--- core/storage/src/main/java/io/github/definitelystable/spongetube/core/storage/ExtentWriter.kt
+++ core/storage/src/main/java/io/github/definitelystable/spongetube/core/storage/ExtentWriter.kt
@@ -174,7 +174,7 @@ internal class ExtentWriter(
 
             store.assertPublishable(stored)
 
-            store.durabilityOps.installAtomicallyNoReplace(
+            store.durabilityOps.installAtomically(
                 partFile,
                 finalFile,
             )
